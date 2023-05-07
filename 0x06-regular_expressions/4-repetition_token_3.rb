c279fdc0e7dc154142dd16c5242191c148977e9c
Elocodes <elochukwuodoeze@gmail.com>
2023-05-06 17:53:08 -0700

    replaced boundaries with *

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -2,4 +2,4 @@
 #script finds pattern that repeat "t" a min of 1 time and max of 4.
 #However, t is not compulsory and a pattern like hbn should be returned too
 
-puts ARGV[0].scan(/hbt?{1,4}n/).join
+puts ARGV[0].scan(/hbt*n/).join
