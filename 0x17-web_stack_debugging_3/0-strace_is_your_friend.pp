67b57c827993003dc74d74b19c3cbea8217b9a2e
Elocodes <elochukwuodoeze@gmail.com>
2023-10-11 15:08:35 -0700

    puppet script to fix error 500 on apache

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# puppet fixes apache 500 error found through strace
+
+exec {'fix typo err':
+    command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+    path    => "/bin";
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,5 @@
+# Web stack debugging 3
+
+# Task
+Using strace, find out why Apache is returning a 500 error.
+The puppet file fixes this error
