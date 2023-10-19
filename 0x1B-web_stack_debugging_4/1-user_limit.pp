9103bdfca8f9f3a948d7c396fccfaba81e289e88
Elocodes <elochukwuodoeze@gmail.com>
2023-10-19 18:20:42 +0000

    adjusted commas around the user path

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,11 @@
 # increasing limit in OS configuration files to enable user "holberton" login and open file without error 
 
 exec {'increase-soft-limit':
-    command  => 'sed -i "holberton soft/s/4/4096/" /etc/security/limits.conf',
-    provider => 'shell'
+    command  => "sed -i '/holberton/s/4/4096/' /etc/security/limits.conf",
+    provider => 'shell',
 }
 
 exec {'increase-hard-limit':
-    command  => 'sed -i "holberton hard/s/5/4096/" /etc/security/limits.conf',
-    provider => 'shell'
+    command  => "sed -i '/holberton/s/5/4096/' /etc/security/limits.conf",
+    provider => 'shell',
 }
